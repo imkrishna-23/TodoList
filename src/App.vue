6b9e4d64097fd92bb791685f69c3f92c4972e622
Your Name <you@example.com>
2024-03-13 11:35:44 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="bg-green-600 text-black p-4 m-4">
+      <h1 class="text-center text-lg md:text-2xl">To Do List</h1>
+    </div>
+    <div class="p-4">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+/* You can add scoped styles here if needed */
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-blue-400 p-2 m-auto text-lg flex justify-evenly">
+        <div class="hover:bg-blue-600 hover:text-white p-2 rounded">
+            <RouterLink to="/add-task">Add task</RouterLink>
+        </div>
+        <div class="hover:bg-blue-600 hover:text-white p-2 rounded">
+            <RouterLink to="/show-task">Show all task</RouterLink>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Navigation'
+    }
+</script>
